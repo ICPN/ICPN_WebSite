4b95cf1d7fd0bc3a90fd5bc7546d27622e3f3206
Yintong <zhouyintong96@gmail.com>
2020-08-08 18:38:22 +0200

    add php partials

---
+++ partials/header.php
@@ -0,0 +1,28 @@
+<header id="header" class="fixed-top header-transparent">
+    <div class="container">
+
+      <div class="logo float-left">
+        <h1 class="text-light"><a href="index.html"><span>ICPN</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu float-right d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Home</a></li>
+          <li class="drop-down"><a href="chisiamo.html">Chi Siamo</a>
+            <ul>
+              <li><a href="missione.html">Missione</a></li>
+              <li><a href="team.html">Team</a></li>
+            </ul>
+          </li>
+          <li><a href="notizie.html">Notizie</a></li>
+          <li><a href="opportunita.html">Opportunità</a></li>
+          <li><a href="iniziative.html">Iniziative</a></li>
+          <li><a href="Eventi.html">Eventi</a></li>
+          <li><a href="contatti.html">Contatti</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
