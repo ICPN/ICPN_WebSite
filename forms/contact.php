1cdca87eb0aedec7a200fd12cd067b0ae5ec2ffa
wkw99 <wkw.angelo@gmail.com>
2020-08-10 11:53:53 +0200

    aggiunta mailer php

--- forms/contact.php
+++ forms/contact.php
@@ -5,37 +5,45 @@
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require '../assets/vendor/phpMailer/Exception.php';
+require '../assets/vendor/phpMailer/PHPMailer.php';
+require '../assets/vendor/phpMailer/SMTP.php';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+$mail = new PHPMailer(true);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+try {
+  //Server settings
+  $mail->isSMTP();                                            // Send using SMTP
+  $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+  $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+  $mail->Username   = 'icpnform@gmail.com';                     // SMTP username
+  $mail->Password   = 'z123789456';                               // SMTP password
+  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+  $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  //Recipients
+  $mail->setFrom('icpnform@gmail.com', 'Web Site');
+  $mail->addAddress('wkw.angelo@gmail.com', 'ICPN');     // Add a recipient
+  //$mail->addAddress('ellen@example.com');               // Name is optional
+  //$mail->addReplyTo('info@example.com', 'Information');
+  //$mail->addCC('cc@example.com');
+  //$mail->addBCC('bcc@example.com');
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  // Attachments
+ // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+  //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // Content
+  $mail->isHTML(true);                                  // Set email format to HTML
+  $mail->Subject = $_POST['subject'];
+  $mail->Body    = "Name: ".$_POST['name']."<br/>Email: ".$_POST['email']."<br/>Testo:".$_POST['message'];
+  $mail->AltBody = "Name: ".$_POST['name']."<br/>Email: ".$_POST['email']."<br/>Testo:".$_POST['message'];
 
-  echo $contact->send();
+  $mail->send();
+  echo 'Message has been sent';
+} catch (Exception $e) {
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
