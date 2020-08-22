7e5a1cd10e536a092fcb4d0f77489e6beb9dcb4b
Yintong <zhouyintong96@gmail.com>
2020-08-22 15:50:31 +0200

    update contents

--- chisiamo.php
+++ chisiamo.php
@@ -52,17 +52,52 @@
               assieme alle competenze specifiche e alla conoscenza delle lingue italiana, cinese ed inglese ci poniamo
               sul mondo professionale e imprenditoriale in Italia, Cina e nel resto del mondo.
             </p>
-            <ul>
-              <li><i class="icofont-check-circled"></i> NETWORKING</li>
-              <li><i class="icofont-check-circled"></i> MENTORING</li>
-              <li><i class="icofont-check-circled"></i> OPPORTUNITÀ</li>
-              <li><i class="icofont-check-circled"></i> RECRUITING</li>
-            </ul>
           </div>
         </div>
 
       </div>
     </section><!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section class="services">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box icon-box-blue">
+              <div class="icon"><i class="bx bx-world"></i></div>
+              <h4 class="title"><a href="">NETWORKING</a></h4>
+              <p class="description">Attraverso l’organizzazione di eventi cercare di facilitare la conoscenza tra persone con interessi e aree di competenze comuni.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box icon-box-green">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4 class="title"><a href="">MENTORING</a></h4>
+              <p class="description">Offrire ai nostri membri opportunità di mentoring che si traducono nella condivisione di consigli professionali basati sul nostro percorso accademico e background professionale.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box icon-box-cyan">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4 class="title"><a href="">OPPORTUNITÀ</a></h4>
+              <p class="description">Mettere tra di loro in contatto aziende o investitori con le conoscenze e capacità necessarie per intraprendere interessanti opportunità di business.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="icon-box icon-box-pink">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">RECRUITING</a></h4>
+              <p class="description">Supportare le aziende che ci contattano cercando di fornire loro i profili dei candidati con i requisiti richiesti.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
 
     <!-- ======= Facts Section ======= -->
     <section class="facts section-bg" data-aos="fade-up">
@@ -85,30 +120,51 @@
       </div>
     </section><!-- End Facts Section -->
 
-    <!-- ======= Our Skills Section ======= -->
-    <section class="skills" data-aos="fade-up">
+    <!-- Service Details Section -->
+    <section class="service-details">
       <div class="container">
 
-        <div class="section-title">
-          <h2>La nostra missione</h2>
-          <p class="text-justify">La nostra missione è creare una comunità di persone, accomunate da un forte interesse
-            e senso di appartenenza alla Cina e all’Italia, che siano interessate o abbiano già incominciato ad
-            intraprendere una carriera professionale.
-            Il network promuove il dialogo attivo e funge da punto di contatto tra studenti, professionisti e aziende
-            per la condivisione di opportunità.
-            Crediamo fortemente nell’importanza dell’educazione, della cultura, dello studio, della diversità e del
-            rispetto. Seguendo questi principi, assieme alle competenze specifiche e alla padronanza delle lingue
-            italiana, cinese ed inglese ci poniamo inoltre sul mondo del lavoro in Italia, Cina e nel resto del mondo. ​
-            Attualmente, fanno parte del nostro network le persone di origine cinese e non, tra cui studenti
-            liceali e universitari, studenti internazionali, professionisti e imprenditori che condividono l’importanza
-            della formazione.
-            Il background accademico dei nostri associati è molto variegato ed eterogeneo, ma sono la competenza e la
-            forte internazionalizzazione la loro caratteristica comune.
-            Tra le principali discipline degli associati, riportiamo Economia, Finanza, Marketing, Ingegneria, Lingue e
-            Giurisprudenza.</p>
+        <div class="row">
+          <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/service-details-1.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="#">MISSIONE</a></h5>
+                <p class="card-text">La nostra missione è creare una comunità di persone, accomunate da un forte interesse
+                  e senso di appartenenza alla Cina e all’Italia, che siano interessate o abbiano già incominciato ad
+                  intraprendere una carriera professionale.
+                  Il network promuove il dialogo attivo e funge da punto di contatto tra studenti, professionisti e aziende
+                  per la condivisione di opportunità.
+                  Crediamo fortemente nell’importanza dell’educazione, della cultura, dello studio, della diversità e del
+                  rispetto. Seguendo questi principi, assieme alle competenze specifiche e alla padronanza delle lingue
+                  italiana, cinese ed inglese ci poniamo inoltre sul mondo del lavoro in Italia, Cina e nel resto del mondo. ​
+                  Attualmente, fanno parte del nostro network le persone di origine cinese e non, tra cui studenti
+                  liceali e universitari, studenti internazionali, professionisti e imprenditori che condividono l’importanza
+                  della formazione.
+                  Il background accademico dei nostri associati è molto variegato ed eterogeneo, ma sono la competenza e la
+                  forte internazionalizzazione la loro caratteristica comune.
+                  Tra le principali discipline degli associati, riportiamo Economia, Finanza, Marketing, Ingegneria, Lingue e
+                  Giurisprudenza.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/service-details-3.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="#">VISIONE</a></h5>
+                <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicaboSed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicaboSed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicaboSed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
+              </div>
+            </div>
+          </div>
         </div>
+
       </div>
-    </section><!-- End Our Skills Section -->
+    </section><!-- End Service Details Section -->
 
   </main><!-- End #main -->
 
