9bea3f75c65ba51359c55aed95cfcbb1938cab74
Yintong <zhouyintong96@gmail.com>
2020-11-04 19:50:21 +0100

    add esports news

---
+++ news/esports.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>E-Sport: come si sta evolvendo il settore - ICPN</title>
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+
+    <?php include '../news/partials/head-style.php'; ?>
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <?php include '../news/partials/header.php'; ?>
+
+    <main id="main">
+
+        <!-- ======= Blog Section ======= -->
+        <section class="breadcrumbs">
+            <div class="container">
+
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2> </h2>
+
+                    <ol>
+                        <li><a href="../index.php">Home</a></li>
+                        <li><a href="../notizie.php">News</a></li>
+                        <li>ICPN: E-Sport: come si sta evolvendo il settore</li>
+                    </ol>
+                </div>
+
+            </div>
+        </section><!-- End Blog Section -->
+
+        <!-- ======= Blog Section ======= -->
+        <section class="blog" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-lg-8 entries">
+
+                        <article class="entry entry-single">
+                           
+                            <h2 class="entry-title">
+                                <a href="#">E-Sport: come si sta evolvendo il settore</a>
+                            </h2>
+
+                            <div class="entry-meta">
+                                <ul>
+                                    <li class="d-flex align-items-center"><i class="icofont-user"></i> <a
+                                            href="../team.php">ICPN</a></li>
+                                    <li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a
+                                            href="#">15 September 2020</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="entry-content text-justify">
+                                <h3>Intervista speciale con Shadow di Mad Lions</h3>
+                                <p>
+                                    L’E-Sport, la disciplina legata ai videogiochi praticato a livello competitivo,
+                                    nutre da diversi decenni un grande interesse e attenzione da parte dei giovani,
+                                    soprattutto negli ultimi anni ha iniziato a crescere esponenzialmente fino a
+                                    raggiungere un impatto anche a livello mondiale, nel 2020 si stima un numero di
+                                    oltre 2,5 miliardi di videogiocatori.
+                                    Questa crescita è data sicuramente dalla facilità di engagement che questa forma di
+                                    intrattenimento riesce a portare con i suoi contenuti adatti sia a chi si affaccia
+                                    per la prima volta questo mondo e sia a chi ha già dimestichezza nel settore
+                                    videoludico e cerca competitività.
+                                </p>
+
+                                <p>
+                                    Il fenomeno dello sport elettronico oggi giorno sta sempre più cadendo agli occhi
+                                    delle varie società sportive anche quelle più rilevanti.
+                                    A dimostrarlo c’è la holding cinese “Suning”, azionista di maggioranza dell’Inter,
+                                    nota squadra di calcio di serie A. Con la sua omonima squadra di League of Legends,
+                                    quest’anno sono riusciti a competere nel finale di League of Legends World
+                                    Championship 2020.
+                                    Inoltre, non possiamo non citare Paris-Saint-Germain (PSG), una società calcistica
+                                    francese, che con la sua squadra PSG Talon, si è qualificato ai mondiali di League
+                                    of Legends di quest’anno uscendo nella fase a gironi.
+                                </p>
+                                <p>
+                                    League of Legends è sicuramente uno dei titoli più blasonati a livello mondiale, un
+                                    classico della scena competitiva dell’E-Sport, con più di un decennio di successi
+                                    alle spalle. Recentemente si è appena concluso League of Legends World, il
+                                    campionato mondiale più importante di League of Legends, nella quale partecipano le
+                                    migliori squadre provenienti dai diversi continenti e si sfidano all’ultimo sangue
+                                    per aggiudicarsi il titolo di migliore squadra dell’anno.
+                                </p>
+
+                                <img src="../assets/img/news/esports1.jpg" alt="" class="img-fluid"><br><br>
+
+                                <p>
+                                    Per quest'anno Legends World Championship, nonostante il Covid-19, si è comunque
+                                    tenuta nei tempi e l’evento è stato organizzato nel nuovo Pudong Stadium di
+                                    Shanghai. Le gare, soprattutto quelle viste dagli spettatori che hanno assistito in
+                                    diretta da casa, erano anche arricchite da effetti speciali tecnologie di ultima
+                                    generazione di ultima generazione.
+                                </p>
+
+                                <img src="../assets/img/news/esports2.jpg" alt="" class="img-fluid"><br><br>
+
+                                <p>
+                                    Anche in Italia il settore videoludico è molto in crescita e non mancano i giocatori
+                                    che competono ad alti livelli, tra cui un ragazzo che nel suo primo anno di esordio
+                                    nella scena competitiva di primissimo livello europeo “LEC” ha fatto insieme alla
+                                    sua squadra “ MAD Lions” una stagione impressionante, arrivando terzi nei playoff
+                                    dello “spring split”, quarti nei playoff della “summer split” e aggiudicandosi un
+                                    biglietto per i mondiali 2020, uscendo nella fase dei gironi di qualificazione.
+                                    Questo ragazzo si chiama Zhiqiang Zhao aka “Shadow”. E’ nato in italia e ha origini
+                                    cinesi, ed è giovanissimo: 19 anni, ha iniziato giocare come giocatore professionale
+                                    all’età di 16. Noi di ICPN abbiamo avuto la possibilità di conoscerlo e farci due
+                                    chiacchiere, andiamo a sentire insieme come Shadow ha approcciato nel suo modo il
+                                    mondo dell’E-sport.
+                                </p>
+
+                                <div class="why-us section-bg" data-aos="fade-up" date-aos-delay="200">
+                                    <div class="container">
+
+                                        <div class="row">
+                                            <div class="col-lg-12 video-box">
+                                                <!--    <img src="../assets/img/news/hschoolhub.jpg" class="img-fluid" alt="">
+                                            <a href="../assets/intervistagamer.mov" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>-->
+                                                <video class="embed-responsive embed-responsive-16by9" width="auto"
+                                                    height="400" controls>
+                                                    <source src="../assets/intervistagamer.mov" type="video/mp4">
+                                                </video>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                        </article><!-- End blog entry -->
+
+                    </div><!-- End blog entries list -->
+
+                </div><!-- End row -->
+
+            </div><!-- End container -->
+
+        </section><!-- End Blog Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <?php include '../news/partials/footer.php'; ?>
+
+    <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+    <?php include '../news/partials/foot-script.php'; ?>
+
+</body>
+
+</html>
--- notizie.php
+++ notizie.php
@@ -45,15 +45,30 @@
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"><a href="news/highschoolhub.php">High School Hub</a></h5>
-                <p class="card-text">High School Hub si rivolge a tutti i ragazzi frequentanti le scuole superiori, che siano licei, istituti tecnici, professionali o accademie, con l’obiettivo di creare una rete estesa a livello nazionale che possa unire studenti di origini italo-cinesi o con uno spiccato interesse verso la Cina in un’unica piattaforma.</p>
-                <div class="read-more"><a href="news/highschoolhub.php"><i class="icofont-arrow-right"></i> Read More</a></div>
+                <h5 class="card-title"><a href="news/highschoolhub.php">E-Sport: come si sta evolvendo
+                    il settore</a></h5>
+                <p class="card-text">L’E-Sport, la disciplina legata ai videogiochi praticato a livello
+                  competitivo, nutre da diversi decenni un grande interesse e attenzione da parte dei
+                  giovani, soprattutto negli ultimi anni ha iniziato a crescere esponenzialmente fino
+                  a raggiungere un impatto anche a livello mondiale, nel 2020 si stima un numero di
+                  oltre 2,5 miliardi di videogiocatori.</p>
+                <div class="read-more"><a href="news/esports.php"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
           </div>
-          
+
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-            
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title"><a href="news/highschoolhub.php">High School Hub</a></h5>
+                <p class="card-text">High School Hub si rivolge a tutti i ragazzi frequentanti le scuole
+                  superiori, che siano licei, istituti tecnici, professionali o accademie, con
+                  l’obiettivo di creare una rete estesa a livello nazionale che possa unire studenti
+                  di origini italo-cinesi o con uno spiccato interesse verso la Cina in un’unica
+                  piattaforma.</p>
+                <div class="read-more"><a href="news/highschoolhub.php"><i class="icofont-arrow-right"></i> Read More</a></div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -63,18 +78,26 @@
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"><a href="initiatives/progettodonazioni.php">Progetto Donazioni</a></h5>
-                <p class="card-text">L’inizio del 2020 è stato caratterizzato da una sequenza di eventi che hanno subito una risonanza globale, l’avvento di un nuovo virus di veloce contagio che si è sparso velocemente in tutto il mondo in poco più di qualche mese, stiamo parlando del coronavirus Sars-CoV-2.</p>
+                <h5 class="card-title"><a href="initiatives/progettodonazioni.php">Progetto
+                    Donazioni</a></h5>
+                <p class="card-text">L’inizio del 2020 è stato caratterizzato da una sequenza di eventi
+                  che hanno subito una risonanza globale, l’avvento di un nuovo virus di veloce
+                  contagio che si è sparso velocemente in tutto il mondo in poco più di qualche mese,
+                  stiamo parlando del coronavirus Sars-CoV-2.</p>
                 <div class="read-more"><a href="initiatives/progettodonazioni.php"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
           </div>
-          
+
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title"><a href="news/comunicato3101.php">Comunicato 31/01</a></h5>
-                <p class="card-text">Apprendiamo con rammarico che il nuovo Corona Virus è arrivato anche in Italia: i due casi confermati sono adesso tenuti sotto stretta sorveglianza dalle autorità sanitarie. Maggiori informazioni sulla possibilità di altri contagi verranno, molto probabilmente, annunciati tra circa 14 giorni, poiché quello è il tempo d’incubazione del virus.</p>
+                <p class="card-text">Apprendiamo con rammarico che il nuovo Corona Virus è arrivato
+                  anche in Italia: i due casi confermati sono adesso tenuti sotto stretta sorveglianza
+                  dalle autorità sanitarie. Maggiori informazioni sulla possibilità di altri contagi
+                  verranno, molto probabilmente, annunciati tra circa 14 giorni, poiché quello è il
+                  tempo d’incubazione del virus.</p>
                 <div class="read-more"><a href="news/comunicato3101.php"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
@@ -87,8 +110,11 @@
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"><a href="news/comunicatocongiunto.php">Comunicato stampa congiunto</a></h5>
-                <p class="card-text">In riferimento alla recente emergenza sanitaria che ha colpito la Cina desideriamo esprimere la nostra vicinanza a tutti coloro che ne sono stati colpiti, anche in modo indiretto.</p>
+                <h5 class="card-title"><a href="news/comunicatocongiunto.php">Comunicato stampa
+                    congiunto</a></h5>
+                <p class="card-text">In riferimento alla recente emergenza sanitaria che ha colpito la
+                  Cina desideriamo esprimere la nostra vicinanza a tutti coloro che ne sono stati
+                  colpiti, anche in modo indiretto.</p>
                 <div class="read-more"><a href="news/comunicatocongiunto.php"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
@@ -97,8 +123,11 @@
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"><a href="news/nuovepartnership.php">Cinque Nuove Partnership per il Futuro degli Studenti Italiani e Stranieri</a></h5>
-                <p class="card-text">Crediamo fermamente che nella capacità di comprendere, accettare e fondere le diversità culturali e professionali sia dell’Italia che della Cina, risieda una delle chiavi principali per costruire il proprio futuro.</p>
+                <h5 class="card-title"><a href="news/nuovepartnership.php">Cinque Nuove Partnership per
+                    il Futuro degli Studenti Italiani e Stranieri</a></h5>
+                <p class="card-text">Crediamo fermamente che nella capacità di comprendere, accettare e
+                  fondere le diversità culturali e professionali sia dell’Italia che della Cina,
+                  risieda una delle chiavi principali per costruire il proprio futuro.</p>
                 <div class="read-more"><a href="news/nuovepartnership.php"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
@@ -112,8 +141,11 @@
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title"><a href="news/casohuawei.php">Il caso Huawei</a></h5>
-                <p class="card-text">Tutto è iniziato verso la fine dell’anno 2018 con l’arresto in Canada di Mèng Wǎn Zhōu, CFO del gruppo Huawei nonchè la figlia del fondatore Rén Zhèng Fēi, la quale è stata accusata per...</p>
-                <div class="read-more"><a href="news/casohuawei.php"><i class="icofont-arrow-right"></i> Read More</a></div>
+                <p class="card-text">Tutto è iniziato verso la fine dell’anno 2018 con l’arresto in
+                  Canada di Mèng Wǎn Zhōu, CFO del gruppo Huawei nonchè la figlia del fondatore Rén
+                  Zhèng Fēi, la quale è stata accusata per...</p>
+                <div class="read-more"><a href="news/casohuawei.php"><i class="icofont-arrow-right"></i>
+                    Read More</a></div>
               </div>
             </div>
           </div>
@@ -121,8 +153,12 @@
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"><a href="news/nuovaviadiseta.php">Nuova via della seta – cosa aspettarsi per l’Italia?</a></h5>
-                <p class="card-text">Il Memorandum d’intesa firmato fra Italia e Cina il 23 marzo 2019 ha sancito l’adesione dell’Italia al progetto One Belt One Road annunciato dalla Cina nel 2013. Il progetto per la Cina risulta sicuramente molto interessante, non solo da un punto di vista economico ma anche politico.</p>
+                <h5 class="card-title"><a href="news/nuovaviadiseta.php">Nuova via della seta – cosa
+                    aspettarsi per l’Italia?</a></h5>
+                <p class="card-text">Il Memorandum d’intesa firmato fra Italia e Cina il 23 marzo 2019
+                  ha sancito l’adesione dell’Italia al progetto One Belt One Road annunciato dalla
+                  Cina nel 2013. Il progetto per la Cina risulta sicuramente molto interessante, non
+                  solo da un punto di vista economico ma anche politico.</p>
                 <div class="read-more"><a href="news/nuovaviadiseta.php"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
@@ -135,8 +171,12 @@
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"><a href="news/education4future.php">Educating for the Future 教育面向未来</a></h5>
-                <p class="card-text">Noi di ICPN siamo stati invitati come guest speaker assieme a società internazionali di primissimo livello come Huawei Italia e lo studio大成-Dentons, l’università LUISS e l’associazione degli studenti cinesi della Bocconi (BCSA).</p>
+                <h5 class="card-title"><a href="news/education4future.php">Educating for the Future
+                    教育面向未来</a></h5>
+                <p class="card-text">Noi di ICPN siamo stati invitati come guest speaker assieme a
+                  società internazionali di primissimo livello come Huawei Italia e lo
+                  studio大成-Dentons, l’università LUISS e l’associazione degli studenti cinesi della
+                  Bocconi (BCSA).</p>
                 <div class="read-more"><a href="news/education4future.php"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
@@ -145,8 +185,11 @@
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"><a href="news/intervistaradiopopolare.php">Intervista Radio Popolare</a></h5>
-                <p class="card-text">Sentirsi 100% Italiani e 100% Cinesi, il modo c’è e si chiama ICPN. Nell’intervista rilasciata a RadioPopolareMilano molti i temi toccati: dalla nascita di ICPN al mostruoso tasso d’abbandono degli studenti Italo-Cinesi.</p>
+                <h5 class="card-title"><a href="news/intervistaradiopopolare.php">Intervista Radio
+                    Popolare</a></h5>
+                <p class="card-text">Sentirsi 100% Italiani e 100% Cinesi, il modo c’è e si chiama ICPN.
+                  Nell’intervista rilasciata a RadioPopolareMilano molti i temi toccati: dalla nascita
+                  di ICPN al mostruoso tasso d’abbandono degli studenti Italo-Cinesi.</p>
                 <div class="read-more"><a href="news/intervistaradiopopolare.php"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
